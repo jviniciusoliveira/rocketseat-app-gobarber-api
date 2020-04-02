8144c7c78d0d49fd042b19c7f330ad0ab08122a0
Jose Vinicius <jviniciusoliveira@gmail.com>
2020-04-02 19:53:54 -0300

    feat: user update

--- back-end/src/app/controllers/user.js
+++ back-end/src/app/controllers/user.js
@@ -4,10 +4,10 @@ class UserController {
   async store(request, response) {
     const { name, email, password, provider = false } = request.body;
 
-    const emailExists = await User.findOne({ where: { email } });
+    const userExists = await User.findOne({ where: { email } });
 
-    if (emailExists) {
-      return response.status(403).json({ error: 'User already exists.' });
+    if (userExists) {
+      return response.status(400).json({ error: 'User already exists.' });
     }
 
     const user = await User.create({
@@ -18,6 +18,33 @@ class UserController {
     });
 
     return response.json(user.id);
+  }
+
+  async update(request, response) {
+    const { email, oldPassword } = request.body;
+
+    const user = await User.findByPk(request.userId);
+
+    if (email !== user.email) {
+      const userExists = await User.findOne({ where: { email } });
+
+      if (userExists) {
+        return response.status(400).json({ error: 'User already exists.' });
+      }
+    }
+
+    if (oldPassword && !(await user.checkPassword(oldPassword))) {
+      return response.status(401).json({ error: 'Password does not match.' });
+    }
+
+    const { id, name, provider } = await user.update(request.body);
+
+    return response.json({
+      id,
+      name,
+      email,
+      provider,
+    });
   }
 }
 
--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -3,9 +3,12 @@ import { Router } from 'express';
 import UserController from './app/controllers/user';
 import SessionController from './app/controllers/session';
 
+import authMiddleware from './app/middlewares/auth';
+
 const router = new Router();
 
 router.post('/users', UserController.store);
+router.put('/users', authMiddleware, UserController.update);
 
 router.post('/sessions', SessionController.store);
 
