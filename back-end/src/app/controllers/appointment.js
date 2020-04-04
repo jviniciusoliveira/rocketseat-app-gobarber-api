0826bb1492d8cca04b77c79787f02b839b7dd48f
Jose Vinicius <jviniciusoliveira@gmail.com>
2020-04-04 16:52:33 -0300

    feat: list of appointments.

--- back-end/src/app/controllers/appointment.js
+++ back-end/src/app/controllers/appointment.js
@@ -2,8 +2,36 @@ import * as Yup from 'yup';
 import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Appointment from '../models/appointment';
 import User from '../models/user';
+import File from '../models/file';
 
 class AppointmentController {
+  async index(request, response) {
+    const appointments = await Appointment.findAll({
+      where: {
+        user_id: request.userId,
+        canceled_at: null,
+      },
+      attributes: ['id', 'date'],
+      order: ['date'],
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+
+    return response.json(appointments);
+  }
+
   async store(request, response) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
