d111fdaec6c3061526b492482674f1d4b5af3119
Jose Vinicius <jviniciusoliveira@gmail.com>
2020-04-04 16:17:48 -0300

    feat: add model and controller for appointments.

---
+++ back-end/src/app/controllers/appointment.js
@@ -0,0 +1,38 @@
+import * as Yup from 'yup';
+import Appointment from '../models/appointment';
+import User from '../models/user';
+
+class AppointmentController {
+  async store(request, response) {
+    const schema = Yup.object().shape({
+      provider_id: Yup.number().required(),
+      date: Yup.date().required(),
+    });
+
+    if (!(await schema.isValid(request.body))) {
+      return response.status(400).json({ error: 'Validation fails.' });
+    }
+
+    const { provider_id, date } = request.body;
+
+    const checkIsProvider = await User.findOne({
+      where: { id: provider_id, provider: true },
+    });
+
+    if (!checkIsProvider) {
+      return response
+        .status(401)
+        .json({ error: 'You can only create appointments with providers.' });
+    }
+
+    const appointment = await Appointment.create({
+      user_id: request.userId,
+      provider_id,
+      date,
+    });
+
+    return response.json(appointment);
+  }
+}
+
+export default new AppointmentController();
--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -6,6 +6,7 @@ import UserController from './app/controllers/user';
 import SessionController from './app/controllers/session';
 import FileController from './app/controllers/file';
 import ProviderController from './app/controllers/provider';
+import AppointmentController from './app/controllers/appointment';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -22,5 +23,7 @@ routes.put('/users', UserController.update);
 routes.get('/providers', ProviderController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
+
+routes.post('/appointments', AppointmentController.store);
 
 export default routes;
