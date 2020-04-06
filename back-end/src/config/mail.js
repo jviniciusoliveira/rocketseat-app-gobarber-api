1465230f6afbb36e8cd7f123f580333ada90250e
Jose Vinicius <jviniciusoliveira@gmail.com>
2020-04-06 20:03:55 -0300

    env:  add configuration by environment variables.

--- back-end/src/config/mail.js
+++ back-end/src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '0739d4934e1de2',
-    pass: '4d7e77121d3fa1',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreplay@gobarber.com>',
--- back-end/src/config/sentry.js
+++ back-end/src/config/sentry.js
@@ -1,4 +1,3 @@
 export default {
-  dsn:
-    'https://a2c50f847ac54d4cbd936b009f8775fe@o315905.ingest.sentry.io/5191319',
+  dsn: process.env.SENTRY_DSN,
 };
